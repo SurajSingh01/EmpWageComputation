7df6a5c8f916b1591808fb04ffb9dcda839bc3df
SurajSingh01 <singhsurajd03@gmail.com>
2020-06-04 07:58:27 +0530

    Solving Using Switch Case

--- empWageComput.sh
+++ empWageComput.sh
@@ -6,17 +6,22 @@ echo "Welcome to Employee Wage Computation"
 WAGE_PER_HOURS=20
 WORKING_HOURS=8
 PART_TIME_HOURS=4
+ABSENT=0
+PRESENT=1
+PART_TIME=2
 
 isPresent=$(( RANDOM % 3 ))
-if [ $isPresent -eq 1 ]
-then
-	echo "Present"
-	dilyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
-elif [ $isPresent -eq 2 ]
-then
-	echo "Part time"
-	dailyWage=$(( $WAGE_PER_HOURS * $PART_TIME_HOURS ))
-elif [ $isPresent -eq 0 ]
-then
-	echo "Absent"
-fi
+
+case $isPresent in
+	$PRESENT)
+		echo "Present"
+		dilyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
+		;;
+	$PART_TIME)
+		echo "Part time"
+		dailyWage=$(( $WAGE_PER_HOURS * $PART_TIME_HOURS ))
+		;;
+	$ABSENT)
+		echo "Absent"
+		;;
+esac
