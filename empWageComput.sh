db43fdcd7bab7991682f3959af028b13ea288ac1
SurajSingh01 <singhsurajd03@gmail.com>
2020-06-04 08:15:07 +0530

    Calculation of wage using while loop

--- empWageComput.sh
+++ empWageComput.sh
@@ -13,22 +13,27 @@ WORKING_DAY_PER_MONTH=20
 
 #variable
 totalWage=0
+totalWorkingHrs=0
+totalWorkingDays=0
 
-for (( counter=0; counter<$WORKING_DAYS_PER_MONTH; counter++ )
+while [[ $totalWorkingHrs -lt 100 && $totalWorkingDays -lt 20 ]]
 do
 
 	isPresent=$(( RANDOM % 3 ))
+	((totalWOrkingDays++))
 
 		case $isPresent in
 			$PRESENT)
 				echo "Present"
 				dilyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
 				totalWage=$(( $totalWage + $dailyWage ))
+				totalWorkingHrs=$(( $totalWorkingHrs + $WORKING_HOURS )
 				;;
 			$PART_TIME)
 				echo "Part time"
 				dailyWage=$(( $WAGE_PER_HOURS * $PART_TIME_HOURS ))
 				totalWage=$(( $totalWage + $dailyWage ))
+				totalWorkingHrs=$(( $totalWorkingHrs + $WORKING_HOURS )
 				;;
 			$ABSENT)
 				echo "Absent"
