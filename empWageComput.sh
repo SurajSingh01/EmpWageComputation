e652694fd34ab7f32f325c59e43305a995492049
SurajSingh01 <singhsurajd03@gmail.com>
2020-06-04 07:51:38 +0530

    Calculating part time wage

--- empWageComput.sh
+++ empWageComput.sh
@@ -5,12 +5,17 @@ echo "Welcome to Employee Wage Computation"
 #Constant
 WAGE_PER_HOURS=20
 WORKING_HOURS=8
+PART_TIME_HOURS=4
 
-isPresent=$(( RANDOM % 2 ))
+isPresent=$(( RANDOM % 3 ))
 if [ $isPresent -eq 1 ]
 then
 	echo "Present"
 	dilyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
+elif [ $isPresent -eq 2 ]
+then
+	echo "Part time"
+	dailyWage=$(( $WAGE_PER_HOURS * $PART_TIME_HOURS ))
 elif [ $isPresent -eq 0 ]
 then
 	echo "Absent"
